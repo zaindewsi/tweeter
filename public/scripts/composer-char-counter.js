86a59ed7c876f3ac474b7d1491c5bfb15e52cb0f
Zain Dewsi <zain.dewsi@gmail.com>
2021-05-18 12:48:32 -0400

    decrement counter and change colour

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,18 @@
+$(document).ready(() => {
+  console.log("dom loaded");
+});
+
+$("#tweet-text").on("input", function () {
+  // Counter value
+  const counter = $(this).closest("form").find(".counter")[0];
+  counter.value = 140 - this.value.length;
+
+  // Counter color change
+  counter.value < 0
+    ? $(counter).css("color", "red")
+    : $(counter).css("color", "#545149");
+
+  // Resize input area
+  this.style.height = "1px";
+  this.style.height = this.scrollHeight + "px";
+});
